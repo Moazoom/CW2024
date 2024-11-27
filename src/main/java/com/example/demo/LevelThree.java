9513fbbc364debfb88ca8d6811d4c805b906c917
Moazoom <moazzamcxv@gmail.com>
2024-11-27 18:35:15 +0800

    add a new level based on levelOne as proof of concept

---
+++ src/main/java/com/example/demo/LevelThree.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+public class LevelThree extends LevelParent {
+
+    private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.jpg";
+    private static final String NEXT_LEVEL = "com.example.demo.LevelTwo";
+    private static final int TOTAL_ENEMIES = 3;
+    private static final int KILLS_TO_ADVANCE = 10;
+    private static final double ENEMY_SPAWN_PROBABILITY = .20;
+    private static final int PLAYER_INITIAL_HEALTH = 5;
+    private static boolean nextLevelStarted = false;
+
+    public LevelThree(double screenHeight, double screenWidth) {
+        super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
+    }
+
+    @Override
+    protected void checkIfGameOver() {
+        if (userIsDestroyed()) {
+            loseGame();
+        }
+        else if (userHasReachedKillTarget() && !nextLevelStarted) {
+            goToNextLevel(NEXT_LEVEL);
+            System.out.println("got here");
+            nextLevelStarted = true;
+        }
+    }
+
+    @Override
+    protected void initializeFriendlyUnits() {
+        getRoot().getChildren().add(getUser());
+    }
+
+    @Override
+    protected void spawnEnemyUnits() {
+        int currentNumberOfEnemies = getCurrentNumberOfEnemies();
+        for (int i = 0; i < TOTAL_ENEMIES - currentNumberOfEnemies; i++) {
+            if (Math.random() < ENEMY_SPAWN_PROBABILITY) {
+                double newEnemyInitialYPosition = Math.random() * getEnemyMaximumYPosition();
+                ActiveActorDestructible newEnemy = new EnemyPlane(getScreenWidth(), newEnemyInitialYPosition);
+                addEnemyUnit(newEnemy);
+            }
+        }
+    }
+
+    @Override
+    protected LevelView instantiateLevelView() {
+        return new LevelView(getRoot(), PLAYER_INITIAL_HEALTH);
+    }
+
+    private boolean userHasReachedKillTarget() {
+        return getUser().getNumberOfKills() >= KILLS_TO_ADVANCE;
+    }
+
+}
